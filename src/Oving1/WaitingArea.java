984c028e4b287c557e3f3d3d0e5e0a2e5b2b4171
Per Haagensen <pdhaagensen@gmail.com>
2019-03-18 13:30:35 +0100

    Initial Commit

---
+++ src/Oving1/WaitingArea.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * This class implements a waiting area used as the bounded buffer, in the producer/consumer problem.
+ */
+public class WaitingArea {
+
+    /**
+     * Creates a new waiting area.
+     *
+     * @param size The maximum number of Customers that can be waiting.
+     */
+	
+	private Queue<Customer> q;
+	private int size;
+	
+    public WaitingArea(int size) {
+        q = new LinkedList<Customer>();
+        this.size = size;
+    }
+
+    /**
+     * This method should put the customer into the waitingArea
+     *
+     * @param customer A customer created by Door, trying to enter the waiting area
+     */
+    public synchronized void enter(Customer customer) {
+        // TODO Implement required functionality
+    	while(q.size() >= size) {
+    		try {
+    			customer.wait();
+    		}
+    		catch(Exception e){
+    			e.printStackTrace();
+    		}
+    		
+    	}
+    	
+    	
+    }
+
+    /**
+     * @return The customer that is first in line.
+     */
+    public synchronized Customer next() {
+        // TODO Implement required functionality
+    }
+
+    // Add more methods as you see fit
+}
